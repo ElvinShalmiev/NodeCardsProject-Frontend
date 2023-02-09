2f3353044b0f3614cf1d13500eaa37c5d89e208b
“ElvinShalmiev” <elvin080192@gmail.com.com>
2023-02-09 12:48:55 +0200

    libraries

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,18 @@
+import { useContext } from "react";
 import { NavLink } from "react-router-dom";
+import AuthContext from "../context/AuthContext";
 
 const Navabar = () => {
+  const {isLoggedIn} = useContext(AuthContext)
   return (
     <nav>
       <NavLink to="/about">About</NavLink>
       <NavLink to="/">Home</NavLink>
-      <NavLink to="/register">Register</NavLink>
-      <NavLink to="/login">Login</NavLink>
+
+      {isLoggedIn && <NavLink to="/register">Register</NavLink>}
+      {isLoggedIn && <NavLink to="/login">Login</NavLink>}
     </nav>
   );
 };
 
-export default Navabar;
+export default Navabar
